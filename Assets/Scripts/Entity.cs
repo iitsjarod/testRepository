307a998ae801e7af1fa8d1ecc93bda58674aef4d
iitsjarod <48459424+iitsjarod@users.noreply.github.com>
2019-03-11 19:55:22 -0400

    Added unity project

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+    // Movement
+    public float speed = 10;
+    public float jumpForce = 10;
+    protected float horizontal = 0;
+
+    // Shoot
+    public GameObject projectile;
+    public float projectileForce = 50;
+    public Transform spawnPoint;
+
+    // GroundCheck
+    protected bool isGrounded;
+    protected Rigidbody2D rb;
+
+    // Animation
+    public Animator anim;
+
+    // Death
+    public bool dead = false;
+
+    // Enemy
+    public int enemyLayer;
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public float GetHorizontalSpeed()
+    {
+        return speed * horizontal;
+    }
+    public virtual void Move()
+    {
+        transform.position += speed * horizontal * Vector3.right * Time.deltaTime;
+        if (horizontal > 0.01f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (horizontal < -0.01f)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    public virtual void Jump()
+    {
+        isGrounded = false;
+        rb.AddForce(jumpForce * Vector3.up, ForceMode2D.Impulse);
+    }
+
+    public virtual void Shoot()
+    {
+        GameObject spawnedProjectile = Instantiate(projectile, spawnPoint.position, Quaternion.identity);
+        spawnedProjectile.GetComponent<Rigidbody2D>().AddForce(projectileForce * transform.right, ForceMode2D.Impulse);
+    }
+
+    public virtual void OnGroundHit()
+    {
+        isGrounded = true;
+    }
+
+    public virtual void OnEnemyHit(GameObject enemy)
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            OnGroundHit();
+        }
+        if (collision.gameObject.layer == enemyLayer)
+        {
+            OnEnemyHit(collision.gameObject);
+        }
+    }
+
+    public virtual void Death()
+    {
+
+    }
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity {
+   
+    public int maxJumps = 2;
+    int jumpCount = 0;
+
+    public Vector3 resetPoint;
+
+    public LayerMask boxLayer;
+    protected override void Start()
+    {
+        base.Start();
+        resetPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Move();
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
+        {
+            Jump();
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    public override void Move()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        base.Move();
+        anim.SetBool("isMoving", Mathf.Abs(horizontal) > 0);
+    }
+
+    public override void Jump()
+    {
+        base.Jump();
+        jumpCount++;       
+    }
+
+    public override void OnGroundHit()
+    {
+        base.OnGroundHit();
+        jumpCount = 0;
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        transform.position = resetPoint;
+    }
+
+    public override void OnEnemyHit(GameObject enemy)
+    {
+        base.OnEnemyHit(enemy);
+        BoxCollider2D myCollider = GetComponent<BoxCollider2D>();
+        //BoxCollider2D enemyCollider = enemy.GetComponent<BoxCollider2D>();
+        //float heightDifference = transform.position.y - enemy.transform.position.y;
+        float direction = enemy.transform.position.x - transform.position.x;
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, myCollider.size * 0.9f, 0, new Vector2(direction, 0), 1, boxLayer);
+        if(transform.position.y < enemy.GetComponent<Entity>().anim.transform.position.y || (hit && hit.collider.gameObject == enemy))
+        {
+            Death();
+        }
+        else
+        {
+            enemy.GetComponent<Entity>().Death();
+        }
+    }
+}
